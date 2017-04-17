7127bf48137daef65fe1b30bd43be42200cad59c
mrpatel15 <mrpatel8180@gmail.com>
2017-04-17 14:35:40 -0500

    maven project

---
+++ src/main/java/com/mrpatel/MavenApp.java
@@ -0,0 +1,10 @@
+package com.mrpatel;
+
+public class MavenApp {
+
+	public static void main(String args[]) {
+        char[] nameArray = {'M', 'r', 'u', 'g', 'e', 's', 'h'};
+        String name = new String(nameArray);
+        System.out.println(name);
+    }
+}
---
+++ src/test/java/com/mrpatel/MavenTest.java
@@ -0,0 +1,16 @@
+package com.mrpatel;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MavenTest {
+
+	@Test
+	
+	public void test() {
+	      String str = "Junit is working fine";
+	      assertEquals("Junit is working fine",str);
+	}
+
+}
